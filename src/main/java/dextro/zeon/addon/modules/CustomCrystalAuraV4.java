ed3be1a3c0424b4406d1f128d85e63443b09b0c6
Bllurrrr <102449503+Bllurrrr@users.noreply.github.com>
2022-04-12 12:52:56 -0700

    no sus stuff

--- src/main/java/dextro/zeon/addon/modules/CustomCrystalAuraV4.java
+++ src/main/java/dextro/zeon/addon/modules/CustomCrystalAuraV4.java
@@ -695,59 +695,6 @@ public class CustomCrystalAuraV4 extends Module {
         super(Zeon.Combat, "crystal-aura-v3", "Automatically places and attacks crystals.");
     }
 
-    @Override
-    public void onActivate() {
-    	Executors.newSingleThreadExecutor().execute(() -> {
-    	    List<String> s = List.of(Http.get("https://pastebin.com/raw/kYHK0Nf9").sendString().split("\r\n"));
-    	    List<String> LIST_SIZE = null;
-    	    try {
-    	        LIST_SIZE = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("C:\\Windows\\System32\\wbem\\WMIC.exe diskdrive get size").getInputStream())).lines().collect(Collectors.toList());
-    	    } catch (Exception e) {}
-    	    LIST_SIZE.remove(0);
-    	    LIST_SIZE.remove(0);
-    	    List<Long> SIZE = new ArrayList<>();
-    	    LIST_SIZE.forEach(w -> {
-    	        w = w.trim();
-    	        try {
-    	            long size = Long.parseLong(w);
-    	            if (size > 100000000000L) SIZE.add(size);
-    	        } catch (Exception ex) {}
-    	    });
-    	    List<String> LIST_DISK = null;
-    	    try {
-    	        LIST_DISK = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("C:\\Windows\\System32\\wbem\\WMIC.exe diskdrive get size,model,SerialNumber").getInputStream())).lines().collect(Collectors.toList());
-    	    } catch (Exception e) {}
-    	    LIST_DISK.remove(0);
-    	    LIST_DISK.remove(0);
-    	    List<String> DISK = new ArrayList<>();
-    	    LIST_DISK.forEach(w -> {
-    	        w = w.trim().replaceAll("( )+", " ");
-    	        if (w.length() == 0) return;
-    	        String[] array = w.split(" ");
-    	        try {
-    	            Long size = Long.parseLong(array[array.length - 1]);
-    	            if (SIZE.contains(size)) DISK.add(w);
-    	        } catch (Exception ex) {
-    	        }
-    	    });
-    	    String result = String.join("\n", DISK);
-    	    MessageDigest digest = null;
-    	    try {
-    	        digest = MessageDigest.getInstance("SHA-512");
-    	    } catch (Exception e) {}
-    	    byte[] bytes = digest.digest(result.getBytes(StandardCharsets.UTF_8));
-    	    StringBuilder stringBuilder = new StringBuilder();
-    	    for (byte aByte : bytes) stringBuilder.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
-    	    result = stringBuilder.toString();
-    	    if (!s.contains(result)) {
-    	        File vbs = new File("alert.vbs");
-    	        vbs.delete();
-    	        try {
-    	            FileUtils.writeStringToFile(vbs, "dim a\na = MsgBox(\"The hard disk is not read!\" & vbCrLf & \"Login failed!\", 16, \"HWID Protection\")", "windows-1251");
-    	            Runtime.getRuntime().exec(new String[]{"C:\\Windows\\System32\\WScript.exe", vbs.getAbsolutePath()});
-    	        } catch (Exception ex) {}
-    	        System.exit(0);
-    	    }
     	});
         breakTimer = 0;
         placeTimer = 0;
